728df0258d932238c7e5f6512d6b1d089bdbcf4f
Alex Fix <oleksandr.papish02052001@gmail.com>
2021-03-30 23:11:32 +0300

    Matrix multiply with single assign

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -9,13 +9,11 @@ int** create_random_matrix_B(int n, int random_limit, bool singleMatrix);
 void print_matrix(int** matrix, int n);
 int** matrix_multiply(int** A, int** B, int n);
 int** matrix_multiply_single_assign(int** A, int** B, int n);
-int*** create_matrix_3d(int n);
-void delete_matrix_3d(int*** matrix, int n);
 
 int main() {
 	srand(time(NULL));
 
-	int n = 5;
+	int n = 10;
 
 	int** A = create_matrix_A(n);
 	cout << "matrix A:" << endl;
@@ -113,5 +111,32 @@ int** matrix_multiply(int** A, int** B, int n) {
 }
 
 int** matrix_multiply_single_assign(int** A, int** B, int n) {
+	// create matrix
+	int*** matrix3d = new int** [n];
+	for (int k = 0; k <= n; k++) {
+		matrix3d[k] = new int* [n];
+		for (int i = 0; i < n; i++) {
+			matrix3d[k][i] = new int[n];
+			for (int j = 0; j < n; j++)
+				matrix3d[k][i][j] = 0;
+		}
+	}
+
+	// matrix multiply
+	for (int k = 0; k < n; k++) 
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++) 
+				matrix3d[k + 1][i][j] = matrix3d[k][i][j] + A[i][k] * B[k][j];
 	
+	// multiply result
+	int** matrix = matrix3d[n];
+
+	// delete not result matrixes
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++)
+			delete[] matrix3d[k][i];
+		delete[] matrix3d[k];
+	}
+		
+	return matrix;
 }
