f65df1bff38170d856dbd3a445b7b3d680de307a
alextravolta123321 <alextravolta123321@gmail.com>
2021-04-02 09:35:46 +0300

    Added Wpf project

---
+++ Lab3UI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkAndInputDimention(object sender, KeyEventArgs e)
+        {
+            if ((e.Key < Key.D0 || e.Key > Key.D9) && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9) || 
+                (e.Key == Key.D0 || e.Key == Key.NumPad0) && ((TextBox)sender).Text.Length == 0)
+                e.Handled = true;
+        }
+    }
+}
